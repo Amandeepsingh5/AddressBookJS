b213aa683f4078a353319af21478649f8adc07c9
Amandeepsingh5 <34280127+Amandeepsingh5@users.noreply.github.com>
2020-02-13 22:42:35 -0500

    two

--- src/test/java/buddy/HttpRequestTest.java
+++ src/test/java/buddy/HttpRequestTest.java
@@ -15,7 +15,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = Application.class)
 public class HttpRequestTest {
-
     @LocalServerPort
     private int port;
 
